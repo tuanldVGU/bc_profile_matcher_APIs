5ea1f5a6a6c7f8490704a7211d4c80c7bd52b2ac
tuanldVGU <37131827+tuanldVGU@users.noreply.github.com>
2022-09-22 18:04:09 +0930

    Update port

--- src/index.ts
+++ src/index.ts
@@ -88,7 +88,8 @@ app.post('/movies', jsonParser, function (req: Request, res: Response) {
   })()  
 })
 
-var server = app.listen(8081, function () {
+
+var server = app.listen(process.env.PORT || 8081, function () {
    var host = server.address().address
    var port = server.address().port
    console.log("Example app listening at http://%s:%s", host, port)
